4d2498c1cfd1135cb3abee3643b7e7d731971e5d
DuyhtbSW <duyhtbde160145@fpt.edu.vn>
2024-05-11 23:21:14 +0700

    Summit 1
    Create Layout

---
+++ ValiWebShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ValiWebShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
